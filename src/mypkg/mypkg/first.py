d3472a0c4de8d2effecccd8d47cdc5b853b43d36
charan <mes.charanm@gmail.com>
2025-06-08 16:02:18 +0530

    Initial commit of ROS 2 workspace

---
+++ src/mypkg/mypkg/first.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+import rclpy
+from rclpy.node import Node
+
+class Mynode(Node): # inherits from the node
+
+    def __init__(self):
+        super().__init__("mynode") # acessing all functionalities of the Node(node name) 
+        self.counter_ = 0
+        self.create_timer(1.0, self.timer_callback) # timer callback
+
+    def timer_callback(self):
+        self.get_logger().info("Hello " + str(self.counter_))
+        self.counter_ += 1
+
+def main(args=None):
+    rclpy.init(args=args) #initializing ros2 communications
+    node = Mynode()
+    rclpy.spin(node) #for continously running the node and callbacks
+    rclpy.shutdown()
+
+if __name__ == '__main__': #which will execute in terimnal also
+    main()
